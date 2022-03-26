5294034f2007b1d886fc8354d71c680ea36895ef
rphlmnbt <rphlmnbt@gmail.com>
2022-03-26 10:17:54 +0800

    Bug fixing

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -8,38 +8,41 @@ export default function Dashboard() {
         <Container className='dashboard'>
             <Row className='w-100 '>
                 <Col>
-                    <div className='dashboard-card mx-auto p-3 d-flex justify-content-center align-items-center flex-column'>
-                        <Link to="/checkin" style={{ textDecoration: 'none' }}>
+                    <Link to="/checkin" style={{ textDecoration: 'none' }}>
+                        <div className='dashboard-card mx-auto p-3 d-flex justify-content-center align-items-center flex-column'>
                             <img src={require('../../assets/qr-code.png')} className='dashboard-image' />
                             <h4 className='dashboard-text'>CHECK IN</h4>
-                        </Link>
-                    </div>
+                        </div>
+                    </Link>
                 </Col>
                 <Col>
-                    <div className='dashboard-card mx-auto p-3 d-flex justify-content-center align-items-center flex-column'>
-                        <Link to="/checkout" style={{ textDecoration: 'none' }}>
+                    <Link to="/checkout" style={{ textDecoration: 'none' }}>
+                        <div className='dashboard-card mx-auto p-3 d-flex justify-content-center align-items-center flex-column'>
                             <img src={require('../../assets/check-out.png')} className='dashboard-image' />
                             <h4 className='dashboard-text'>CHECK OUT</h4>
-                        </Link>
-                    </div>
+                        </div>
+                    </Link>
+                    
                 </Col>
             </Row>
             <Row className='w-100'>
-                <Col> 
-                    <div className='dashboard-card mx-auto p-3 d-flex justify-content-center align-items-center flex-column'>
-                        <Link to="reservations" style={{ textDecoration: 'none' }}>
+                
+                <Col>
+                    <Link to="reservations" style={{ textDecoration: 'none' }}>
+                        <div className='dashboard-card mx-auto p-3 d-flex justify-content-center align-items-center flex-column'>
                             <img src={require('../../assets/booking.png')} className='dashboard-image' />
                             <h4 className='dashboard-text'>VIEW RESERVATIONS</h4>
-                        </Link>
-                    </div>
+                        </div>
+                    </Link>
                 </Col>
                 <Col>
-                    <div className='dashboard-card mx-auto p-3 d-flex justify-content-center align-items-center flex-column'>
-                        <Link to="/login" style={{ textDecoration: 'none' }}>
+                    <Link to="/login" style={{ textDecoration: 'none' }}>
+                        <div className='dashboard-card mx-auto p-3 d-flex justify-content-center align-items-center flex-column'>
                             <img src={require('../../assets/librarian.png')} className='dashboard-image' />
                             <h4 className='dashboard-text'>ADMIN TOOLS</h4>
-                        </Link>
-                    </div>    
+                        </div>
+                    </Link>
+                    
                 </Col>
             </Row>
         </Container> 
